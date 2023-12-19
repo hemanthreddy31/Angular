d1eabb377efa8bba894b883b4b70ff8d0cce0414
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2023-12-19 11:09:35 +0530

    Custom Property Binding Input

--- ekart/src/app/container/product-list/product-list.component.ts
+++ ekart/src/app/container/product-list/product-list.component.ts
@@ -532,4 +532,9 @@ export class ProductListComponent {
       slug: "michael-feburary-sk8-hi"
     }
   ];
+
+
+  totalProductCount = this.products.length;
+  totalProductInStock=this.products.filter(p=>p.is_in_inventory === true).length;
+  totalProductsOutOfStock=this.products.filter(p=>p.is_in_inventory === false).length;
 }
