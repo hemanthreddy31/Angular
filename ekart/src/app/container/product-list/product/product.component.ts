9abc62ca515c05290044bb0db7f3557e41cf0da5
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2023-12-20 13:23:01 +0530

    Reference Variable Component

--- ekart/src/app/container/product-list/product/product.component.ts
+++ ekart/src/app/container/product-list/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component,Input } from '@angular/core';
-
+import { Product } from 'src/app/Models/Product';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
