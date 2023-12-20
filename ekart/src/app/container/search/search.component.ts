1411b73ac2451989e72e3e75917899eaffd08d62
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2023-12-20 12:12:08 +0530

    template Reference Variable

--- ekart/src/app/container/search/search.component.ts
+++ ekart/src/app/container/search/search.component.ts
@@ -1,20 +1,23 @@
-import { Component,EventEmitter,Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
-  searchText: string = ''; 
+  searchText: string = '';
 
+  //1.Create an event
+  @Output()
+  searchTextChanged: EventEmitter<string> = new EventEmitter<string>();
 
-//1.Create an event
-@Output()
-searchTextChanged:EventEmitter<string> = new EventEmitter<string>()
+  onSearchTextChanged() {}
 
-onSearchTextChanged(){
-  this.searchTextChanged.emit(this.searchText);
-}
-
+  updateSearchText(inputEl: HTMLInputElement) {
+    //this.searchText=event.target.value;
+    //console.log(inputEl.value);
+    this.searchText = inputEl.value;
+    this.searchTextChanged.emit(this.searchText);
+  }
 }
