738dd4c4a4e055cd7f2dc9ca0b45ae53cc4c69c8
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2023-12-22 14:31:12 +0530

    Component Initialization

---
+++ Angular-Lifecycle-Hook/src/app/demo/demo.component.ts
@@ -0,0 +1,16 @@
+import { Component,Input} from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.css']
+})
+export class DemoComponent{
+  title:string = 'Demo Component';
+  @Input() message:string ='Hello';
+   constructor(){
+    console.log('Demo component constructor called');
+    console.log(this.title);
+    console.log(this.message);
+   }
+}
