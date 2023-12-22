d477f0882fa0e1406596eeb6953afd152c90469a
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2023-12-22 19:48:56 +0530

    Renderer 2

--- ekart/src/app/CustomDirectives/SetBackground.directive.ts
+++ ekart/src/app/CustomDirectives/SetBackground.directive.ts
@@ -1,15 +1,20 @@
-import { Directive, ElementRef, OnInit } from "@angular/core";
+import { Directive, ElementRef, OnInit, Renderer2 } from "@angular/core";
 
 @Directive({
     selector:'[setBackground]'
 })
 export class SetBackground implements OnInit{
     // private element:ElementRef;
-    constructor(private element:ElementRef){
+    //private renderer:Renderer2;
+    constructor(private element:ElementRef, private renderer:Renderer2){
         //this.element=element;
+        //this.renderer=renderer;
     }
     ngOnInit(){
-        this.element.nativeElement.style.backgroundColor='green';
-        this.element.nativeElement.style.color='white';
+        // this.element.nativeElement.style.backgroundColor='green';
+        // this.element.nativeElement.style.color='white';
+        this.renderer.setStyle(this.element.nativeElement,'backgroundColor','green');
+        this.renderer.setStyle(this.element.nativeElement,'color','white');
+       // this.renderer.setAttribute(this.element.nativeElement, 'title','This is Example title');
     }
 }
