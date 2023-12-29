c08811dd0185784a09d0ae8211b6dc4334b2760e
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2023-12-29 16:37:24 +0530

    Creating & Using an Observable

--- AngularObservables/src/app/app.component.ts
+++ AngularObservables/src/app/app.component.ts
@@ -10,8 +10,21 @@ export class AppComponent {
   title = 'AngularObservables';
   data: any[]=[];
 
-
+  //Observable
   myObservable = new Observable((observer)=>{
-      observer.next([1,2,3,4,5]);
+      setTimeout(() => {observer.next(1)},1000);
+      setTimeout(() => {observer.next(2)},2000);
+      setTimeout(() => {observer.next(3)},3000);
+      setTimeout(() => {observer.next(4)},4000);
+      setTimeout(() => {observer.next(5)},5000);
+ 
   });
+   
+  GetAsyncData(){
+    //Observer
+    //next, error , complete
+    this.myObservable.subscribe((val:any)=>{
+      this.data.push(val);
+    });
+  }
 }
