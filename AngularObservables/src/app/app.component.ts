0a6929e8bddaf13f255f1f658d6259bdc414be52
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2023-12-29 16:56:27 +0530

    Error & Completion of Observable

--- AngularObservables/src/app/app.component.ts
+++ AngularObservables/src/app/app.component.ts
@@ -4,27 +4,64 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AngularObservables';
-  data: any[]=[];
+  data: any[] = [];
 
   //Observable
-  myObservable = new Observable((observer)=>{
-      setTimeout(() => {observer.next(1)},1000);
-      setTimeout(() => {observer.next(2)},2000);
-      setTimeout(() => {observer.next(3)},3000);
-      setTimeout(() => {observer.next(4)},4000);
-      setTimeout(() => {observer.next(5)},5000);
- 
+  myObservable = new Observable((observer) => {
+    setTimeout(() => {
+      observer.next(1);
+    }, 1000);
+    setTimeout(() => {
+      observer.next(2);
+    }, 2000);
+    setTimeout(() => {
+      observer.next(3);
+    }, 3000);
+    // setTimeout(() => {
+    //   observer.error(
+    //     new Error('Something Went wrong . Please try again later')
+    //   );
+    // }, 3000);
+    setTimeout(() => {
+      observer.next(4);
+    }, 4000);
+    setTimeout(() => {
+      observer.next(5);
+    }, 5000);
+    setTimeout(() => {
+      observer.complete()
+    }, 6000);
   });
-   
-  GetAsyncData(){
+
+  GetAsyncData() {
     //Observer
     //next, error , complete
-    this.myObservable.subscribe((val:any)=>{
-      this.data.push(val);
-    });
+    // this.myObservable.subscribe(
+    //   (val: any) => {
+    //     this.data.push(val);
+    //   },
+    //   (err) => {
+    //     alert(err.message);
+    //   },
+    //   ()=>{
+    //     alert('All the data is streamed');
+    //   }
+    // );
+
+    this.myObservable.subscribe({
+      next:(val:any)=>{
+        this.data.push(val);
+      },
+      error(err){
+        alert(err.message);
+      },
+      complete(){
+        alert('All the data is streamed!')
+      }
+    })
   }
 }
