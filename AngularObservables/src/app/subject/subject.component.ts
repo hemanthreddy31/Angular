edeba2009774097a3323306c3a2416a1b38a0694
Hemanth Kankanala <hemanth.kankanala@realpage.com>
2024-01-01 14:33:02 +0530

    Promise Vs Observable

--- AngularObservables/src/app/subject/subject.component.ts
+++ AngularObservables/src/app/subject/subject.component.ts
@@ -47,13 +47,34 @@ export class SubjectComponent implements OnInit{
 
 
     //ASYNC Subject
-    const asyncSubject=new AsyncSubject();
-    asyncSubject.next(100);
-    asyncSubject.next(200);
-    asyncSubject.next(300);
-    asyncSubject.complete();
-    asyncSubject.subscribe(data=>console.log(`Subscriber 1: ${data}`));
-    asyncSubject.next(400);
-    asyncSubject.complete();
+    // const asyncSubject=new AsyncSubject();
+    // asyncSubject.next(100);
+    // asyncSubject.next(200);
+    // asyncSubject.next(300);
+    // asyncSubject.complete();
+    // asyncSubject.subscribe(data=>console.log(`Subscriber 1: ${data}`));
+    // asyncSubject.next(400);
+    // asyncSubject.complete();
+
+
+   //Difference between Promise and Observable
+   const promise=new Promise((resolve, reject)=>{
+    console.log('promises called');
+    resolve(100);
+    resolve(200);
+    resolve(300);
+   })
+   promise.then((data)=>{
+    console.log(data);
+   })
+   const obs=new Observable((sub)=>{
+    console.log('observable called');
+    sub.next(100);
+    sub.next(200);
+    sub.next(300);
+   })
+   obs.subscribe(data=>console.log(data));
+
+
   }
 }
